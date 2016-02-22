8901a46713bd5cc346c8aa8e3e2a9285d1860775
Jamie Brown <jamiebrown201@hotmail.com>
2016-02-22 12:34:01 +0000

    server and index.html

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 var express  = require('express');
 var app      = express();
 var mongoose = require('mongoose');
-var morgan = require('morgan');
+var port  	 = process.env.PORT || 8080;
+var morgan   = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+
+
+mongoose.connect('mongodb://jamie:sadman201@apollo.modulusmongo.net:27017/Ybyhys9i');
+app.use(express.static(__dirname));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+
+
+app.get('/', function(req, res) {
+		res.sendfile('./public/index.html');
+});
+
+
+app.listen(port);
+console.log("App listening on port " + port);
